bcb3447d529d60d3da3e189e152f37f3efb28696
alivadjid1 <alivadjid@gmail.com>
2021-01-15 09:57:45 +0300

    ленивая загрузка компонента React.lazy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
-import AddTodo from './Todo/AddTodo'
+// import AddTodo from './Todo/AddTodo'
 import Loader from './Loader'
+
+const AddTodo = React.lazy(
+  () => 
+  new Promise(resolve => setTimeout(() => {
+    resolve(import('./Todo/AddTodo'))
+  }, 3000))
+)
 
 function App() {
   const [todos, setTodos] = React.useState([])
@@ -46,7 +53,10 @@ function App() {
     <Context.Provider value={{removeTodo}}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        < AddTodo onCreate={addTodo}/>
+          <React.Suspense fallback={<Loader />}>
+            < AddTodo onCreate={addTodo}/>
+          </React.Suspense>
+        
 
         { loading && <Loader />}
         { todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : loading ? null :  <p>No todos</p>}

b2b139f62e6ab80c764faa640194a5c5fc11c5aa
alivadjid1 <alivadjid@gmail.com>
2021-01-15 09:34:47 +0300

    useEffect работа с сервером

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    {id: 1, comleted: false, title: 'Изучить react'},
-    {id: 2, comleted: true, title: 'Изучить redux'},
-    {id: 3, comleted: false, title: 'Изучиь node'}
-  ])
+  const [todos, setTodos] = React.useState([])
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => response.json())
+    .then(todos =>{
+      setTimeout(()=>{
+        setTodos(todos)
+      }, 2000)
+      
+    })
+  }, [])
 
   function toggleTodo(id){
     setTodos(todos.map(todo => {
